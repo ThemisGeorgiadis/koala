fdb76e6465e49f9d2aa875f0d7e889fbec0990b0
themis <georgi.themis@gmail.com>
2026-08-02 01:29:49 +0300

    Added Fedora support to dependency setup script

--- weather/install.sh
+++ weather/install.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-sudo apt-get update 
+source /etc/os-release
 
-pkgs="curl wget unzip coreutils gzip gawk sed findutils git python3 python3-pip python3-venv"
+if [[ "$ID" == "fedora" ]]; then
+    INSTALLER="dnf"
+    PACKAGES=(
+        curl wget unzip coreutils gzip gawk sed findutils
+            git python3 python3-pip # python3-venv is included with python3 on Fedora
+    )
+else
+    INSTALLER="apt-get"
+    PACKAGES=(
+        curl wget unzip coreutils gzip gawk sed findutils
+            git python3 python3-pip python3-venv
+    )
+fi
 
-for pkg in $pkgs; do
-    if ! dpkg -l | grep -q "$pkg"; then
-        sudo apt-get install -y --no-install-recommends "$pkg"
+sudo "$INSTALLER" update 
+
+for pkg in "${PACKAGES[@]}"; do
+    if [[ "$ID" == "fedora" ]]; then
+        if ! rpm -q "$pkg" >/dev/null 2>&1; then
+            sudo dnf install -y "$pkg"
+        fi
+    else
+        if ! dpkg -l | grep -q "$pkg"; then
+            sudo apt-get install -y --no-install-recommends "$pkg"
+        fi
     fi
 done
 
