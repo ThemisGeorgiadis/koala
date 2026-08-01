0332d0723ebd8ed6ab4e0b44f5b9fdd9876836ce
themis <georgi.themis@gmail.com>
2026-08-02 00:48:57 +0300

    Add Fedora support to setup script dependencies

--- setup.sh
+++ setup.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 set -e
 
+source /etc/os-release
+
+if [[ "$ID" == "fedora" ]]; then
+    INSTALLER="dnf"
+    PACKAGES=(
+        git procps-ng autoconf automake libtool
+        gcc gcc-c++ make
+        cloc time gawk jq strace lsof
+        python3 python3-pip
+    )
+else
+    INSTALLER="apt-get"
+    PACKAGES=(
+        git procps autoconf automake libtool
+        build-essential
+        cloc time gawk jq strace lsof
+        python3 python3-pip python3-venv
+    )
+fi
+
 cd "$(realpath "$(dirname "$0")")" || exit 1
 
 TOP=$(git rev-parse --show-toplevel)
-sudo apt-get update
-sudo apt-get install -y  git procps autoconf automake libtool build-essential cloc time gawk jq strace lsof python3 python3-pip python3-venv
+sudo "$INSTALLER" update
+sudo "$INSTALLER" install -y "${PACKAGES[@]}"
 VENV_DIR="$TOP/venv"
 rm -rf "$VENV_DIR"
 python3 -m venv "$VENV_DIR"
